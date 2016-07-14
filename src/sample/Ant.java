1234caa505bb1b88c0e1212c53350374b13566b8
jeffryporter <jporter7@gmail.com>
2016-07-14 10:23:28 -0400

    Draw ants

---
+++ src/sample/Ant.java
@@ -0,0 +1,16 @@
+package sample;
+
+/**
+ * Created by jeffryporter on 7/14/16.
+ */
+public class Ant
+{
+    double x;
+    double y;
+
+    public Ant(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
